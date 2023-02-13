23fe322044cc1d978f2d86732da8cdc1ed25d9b6
samu10112 <119002488+samu10112@users.noreply.github.com>
2023-02-13 18:00:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,16 +3,17 @@ var canvas = new fabric.Canvas("myCanvas");
 
 function newImage()
 {
-    fabric.image.fromURL("BirthdayImage.jpg") 
-	blockImageObject = Img;
+    fabric.Image.fromURL("BirthdayImage.jpg", function(Img){
+        	blockImageObject = Img;
 
     blockImageObject.scaleToWidth(700);
     blockImageObject.scaleToHeight(510);
     blockImageObject.set({
     top:0,
-    lest:0
+    left:0
     });
     canvas.add(blockImageObject);
+    });
 }
 
 function playSound(){
